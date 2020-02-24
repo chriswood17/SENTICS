1ccec0b45f872c00858cee966513f2c10c9ebcf2
mattescreen <chriswood.virtual@gmail.com>
2020-02-24 12:41:29 -0600

    php email

--- sentics-email.php
+++ sentics-email.php
@@ -6,6 +6,35 @@
 	$strokeC = htmlspecialchars($_POST['stroke_C']);
 	$strokeD = htmlspecialchars($_POST['stroke_D']);
 
-echo $text, ' ', $strokeA, $strokeB, $strokeC, $strokeD;
+	$email_subject = "SENTICS_input";
+	$email_body = "$text, ' ', 'strokeA:'', $strokeA, ' ', 'strokeB:', $strokeB, ' ', 'strokeC:', $strokeC, ' ', 'strokeD', $strokeD";
+	$to = "mattescreen00@gmail.com";
+	
+	mail($to, $email_subject, $email_body);
+	header('Location: thankyou.html');
+
+	function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+
+//echo $text, ' ', 'strokeA:', $strokeA, ' ', 'strokeB:', $strokeB, ' ', 'strokeC:', $strokeC, ' ', 'strokeD', $strokeD;
 	
 	?>
